d51cea77f6cfdf675e7fbd9631fa4d77b415e2f1
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-24 17:18:40 +0530

    remove ambari-restart

--- start.sh
+++ start.sh
@@ -5,5 +5,3 @@ sudo su
 git clone https://github.com/chuyqa/alluxio-ambari-service /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO
 
 wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO/package/files
-
-ambari-server restart
