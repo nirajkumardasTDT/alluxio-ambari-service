165974f7e6fc519b0bdf41bf3cdcd0a54e5d6f85
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-24 18:25:55 +0530

    req change for ambari

--- start.sh
+++ start.sh
@@ -8,7 +8,6 @@ rm -rf /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/ALLUXIO
 git clone https://github.com/nirajkumardasTDT/alluxio-ambari-service /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO
 git clone https://github.com/nirajkumardasTDT/alluxio-ambari-service /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/ALLUXIO
 
-wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO/package/files
-
-
 wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/ALLUXIO/package/files
+
+wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO/package/files
