39a7cce45bb5c3777bdb438a16f1b2048bee24a5
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-24 17:39:36 +0530

    adding repo to ambari-agent

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@
 sudo su
 
 git clone https://github.com/chuyqa/alluxio-ambari-service /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO
+git clone https://github.com/chuyqa/alluxio-ambari-service /var/lib/ambari-agent/cache/stacks/HDP/2.5/services/ALLUXIO/
 
 wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO/package/files
