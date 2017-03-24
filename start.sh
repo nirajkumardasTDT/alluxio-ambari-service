5568bc5cd1440340fc988fd61e3f2be7d5f10e21
Niraj Kumar Das <niraj.kumar.das@thedatateam.in>
2017-03-24 17:04:43 +0530

    changing version to 2.5

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 sudo su
 
-git clone https://github.com/chuyqa/alluxio-ambari-service /var/lib/ambari-server/resources/stacks/HDP/2.4/services/ALLUXIO
+git clone https://github.com/chuyqa/alluxio-ambari-service /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO
 
 wget http://alluxio.org/downloads/files/1.0.1/alluxio-1.0.1-bin.tar.gz -P /var/lib/ambari-server/resources/stacks/HDP/2.5/services/ALLUXIO/package/files
 
